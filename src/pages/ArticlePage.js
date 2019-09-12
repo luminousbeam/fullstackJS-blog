9f9dbe1ccb681ada2b9d32d8f4bf51e45a1655b5
luminousbeam <jocelyn.jeriah@gmail.com>
2019-09-11 22:10:08 -0400

    pull from article seed list and display url name params

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import articleContent from './article-content'
 
-const ArticlePage = ({ match }) => (
-  <>
-    <h1>This is an Article</h1>
-  </>
-);
+const ArticlePage = ({ match }) => {
+  const name = match.params.name;
+  const article = articleContent.find(article => article.name === name);
+
+  if (!article) return <h1>Article does not exist!</h1>
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+        {article.content.map((paragraph, key) => (
+          <p key={key}>{paragraph}</p>
+        ))}
+    </>
+  );
+}
 
 export default ArticlePage;
