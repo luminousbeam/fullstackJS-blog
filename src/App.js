54b122becc76778d06277212eda1dee803b2b1ae
luminousbeam <jocelyn.jeriah@gmail.com>
2019-09-11 22:29:42 -0400

    create ArticleList component, rename ArticlesListPage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import ArticlePage from './pages/ArticlePage'
-import ArticlesList from './pages/ArticlesList'
+import ArticlesListPage from './pages/ArticlesListPage'
 import NavBar from './NavBar'
 import './App.css';
 
@@ -18,7 +18,7 @@ function App() {
         <div id="page-body">
           <Route path='/' component={HomePage} exact />
           <Route path='/about' component={AboutPage} />
-          <Route path='/articles-list' component={ArticlesList} />
+          <Route path='/articles-list' component={ArticlesListPage} />
           <Route path='/article/:name' component={ArticlePage} />
         </div>
       </div>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import articleContent from './article-content'
+import articleContent from './article-content';
+import ArticlesList from '../components/ArticlesList';
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
@@ -7,12 +8,16 @@ const ArticlePage = ({ match }) => {
 
   if (!article) return <h1>Article does not exist!</h1>
 
+  const otherArticles = articleContent.filter(article => article.name !== name);
+
   return (
     <>
       <h1>{article.title}</h1>
-        {article.content.map((paragraph, key) => (
-          <p key={key}>{paragraph}</p>
-        ))}
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
+      <h3>Other Articles:</h3>
+      <ArticlesList articles={otherArticles} />
     </>
   );
 }
