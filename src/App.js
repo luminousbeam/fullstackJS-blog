718a9a7590c68da800421bfb85364f1934c72309
luminousbeam <jocelyn.jeriah@gmail.com>
2019-09-12 12:30:54 -0400

    create NotFoundPage with 404 error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom';
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import ArticlePage from './pages/ArticlePage'
 import ArticlesListPage from './pages/ArticlesListPage'
+import NotFoundPage from './pages/NotFoundPage'
 import NavBar from './NavBar'
 import './App.css';
 
@@ -16,10 +18,13 @@ function App() {
       <div className="App">
         <NavBar />
         <div id="page-body">
-          <Route path='/' component={HomePage} exact />
-          <Route path='/about' component={AboutPage} />
-          <Route path='/articles-list' component={ArticlesListPage} />
-          <Route path='/article/:name' component={ArticlePage} />
+          <Switch>
+            <Route path='/' component={HomePage} exact />
+            <Route path='/about' component={AboutPage} />
+            <Route path='/articles-list' component={ArticlesListPage} />
+            <Route path='/article/:name' component={ArticlePage} />
+            <Route component={NotFoundPage} />
+          </Switch>
         </div>
       </div>
     </Router>
