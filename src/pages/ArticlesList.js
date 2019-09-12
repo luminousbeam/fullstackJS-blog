2c1dabb146aab51678d56fea9f96e2d6ca16568f
luminousbeam <jocelyn.jeriah@gmail.com>
2019-09-11 22:10:44 -0400

    display articles list from seed, up to 150 chars. link to article

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import articleContent from './article-content';
 
 const ArticlesList = () => (
   <>
     <h1>Articles</h1>
+    {articleContent.map((article, key) => (
+      <Link className="article-list-item" key={key} to={`/article/${article.name}`}>
+        <h3>{article.title}</h3>
+        <p>{article.content[0].substring(0, 150)}...</p>
+      </Link>
+    ))}
   </>
 );
 
