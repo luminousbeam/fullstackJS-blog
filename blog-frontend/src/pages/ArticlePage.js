3e570cfae562e675f35a4d8bc580f82e3207d64f
luminousbeam <jocelyn.jeriah@gmail.com>
2019-09-12 21:23:08 -0400

    move .env back to blog-frontend folder, create React Hooks

--- blog-frontend/src/pages/ArticlePage.js
+++ blog-frontend/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import articleContent from './article-content';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage'
@@ -7,6 +7,12 @@ const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articleContent.find(article => article.name === name);
 
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    setArticleInfo({ upvotes: 12});
+  })
+
   if (!article) return <NotFoundPage />
 
   const otherArticles = articleContent.filter(article => article.name !== name);
@@ -14,6 +20,7 @@ const ArticlePage = ({ match }) => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This article has been upvoted {articleInfo.upvotes} times</p>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
