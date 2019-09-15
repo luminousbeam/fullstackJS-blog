696cf2d6ff54999b436f898879e1ea9ef6ed9346
luminousbeam <jocelyn.jeriah@gmail.com>
2019-09-15 13:01:21 -0400

    finish adding functionality to AddCommentForm and add it to ArticlePage

--- blog-frontend/src/components/AddCommentForm.js
+++ blog-frontend/src/components/AddCommentForm.js
@@ -1,21 +1,35 @@
 import React, { useState } from 'react';
 
-const AddCommentForm = () => {
+const AddCommentForm = ({ articleName, setArticleInfo }) => {
   const [username, setUserName] = useState('');
   const [commentText, setCommentText] =useState('');
-  
+
+  const addComment = async () => {
+    const result = await fetch(`/api/articles/${articleName}/add-comment`, {
+      method: 'post',
+      body: JSON.stringify({ username, text: commentText }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const body = await result.json();
+    setArticleInfo(body);
+    setUserName('');
+    setCommentText('');
+  }
+
   return (
     <div id="add-comment-form">
       <h3>Add a Comment</h3>
       <label>
         Name:
-        <input type="text" />
+        <input type="text" value={username} onChange={(event) => setUserName(event.target.value)} />
       </label>
       <label>
         Comment:
-        <textarea rows="4" cols="50" />
+        <textarea rows="4" cols="50" value={commentText} onChange={(event) => setCommentText(event.target.value)} />
       </label>
-      <button>Add Comment</button>
+      <button onClick={() => addComment()}>Add Comment</button>
     </div>
   )
 };
--- blog-frontend/src/pages/ArticlePage.js
+++ blog-frontend/src/pages/ArticlePage.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import articleContent from './article-content';
 import CommentsList from '../components/CommentsList'
 import ArticlesList from '../components/ArticlesList';
-import UpvotesSection from '../components/UpvotesSection'
-import NotFoundPage from './NotFoundPage'
+import UpvotesSection from '../components/UpvotesSection';
+import AddCommentForm from '../components/AddCommentForm';
+import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
@@ -33,6 +34,7 @@ const ArticlePage = ({ match }) => {
         <p key={key}>{paragraph}</p>
       ))}
       <CommentsList comments={articleInfo.comments} />
+      <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
       <h3>Other Articles:</h3>
       <ArticlesList articles={otherArticles} />
     </>
