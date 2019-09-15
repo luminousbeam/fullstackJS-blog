9642daab2cb93adac7cf9a84ccf114c0097ca029
luminousbeam <jocelyn.jeriah@gmail.com>
2019-09-15 13:45:32 -0400

    prep app for release

---
+++ blog-frontend/build/precache-manifest.3f1682d349234ea0b5edc3a8e03b15e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c514ab3628e4d604531bc4bad35c81f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2a1d457d85b96f4d86e",
+    "url": "/static/css/main.c58b48e1.chunk.css"
+  },
+  {
+    "revision": "7230a11ca141cf90eab6",
+    "url": "/static/js/2.0a5af092.chunk.js"
+  },
+  {
+    "revision": "d2a1d457d85b96f4d86e",
+    "url": "/static/js/main.9c214c78.chunk.js"
+  },
+  {
+    "revision": "ff8fc16a939599321a27",
+    "url": "/static/js/runtime~main.73aae552.js"
+  }
+]);
